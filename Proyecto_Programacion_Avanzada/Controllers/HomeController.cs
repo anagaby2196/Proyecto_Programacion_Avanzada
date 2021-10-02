feec45627c46a7d072440b53f4c055a722491e18
anagaby2196 <80338672+anagaby2196@users.noreply.github.com>
2021-10-02 11:44:28 -0600

    Agregar archivos de proyecto.

---
+++ Proyecto_Programacion_Avanzada/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto_Programacion_Avanzada.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Registro()
+        {
+            return View();
+        }
+
+        public ActionResult Citas()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Nosotros()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Servicios()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Promociones()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Contacto()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Proyecto_Programacion_Avanzada/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Proyecto_Programacion_Avanzada.MvcApplication" Language="C#" %>
