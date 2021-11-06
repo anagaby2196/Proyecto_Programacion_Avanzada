a849309043675a64d68486c595364d9804727c76
JhonselDiaz <jdiazch.21@gmail.com>
2021-11-06 01:09:59 -0600

    Clases en BLL

---
+++ Clinica.BLL/PersonaBLL.cs
@@ -0,0 +1,21 @@
+﻿using Entidades.ETL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clinica.BLL
+{
+    public class PersonaBLL
+    {
+
+        public void RegistrarUsuarioBLL(PersonaETL persona)
+        {
+           //return RegistrarUsuario(persona);
+
+        }
+
+
+    }
+}
