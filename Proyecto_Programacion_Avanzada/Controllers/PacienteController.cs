27ce527fd670271e9e6f69d00e01e36e984aed27
k3vin1995 <57731251+k3vin1995@users.noreply.github.com>
2021-11-10 01:20:39 -0600

    Agregando Cambios

---
+++ Proyecto_Programacion_Avanzada/Controllers/PacienteController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto_Programacion_Avanzada.Controllers
+{
+    public class PacienteController : Controller
+    {
+        // GET: Paciente
+        public ActionResult ConsultaPacientes()
+        {
+            return View();
+        }
+    }
+}
