d744b6f1e8d672635d21b7f00be88cd8a82aabb8
k3vin1995 <57731251+k3vin1995@users.noreply.github.com>
2021-11-21 16:00:38 -0600

    edicion en paciente

---
+++ Proyecto_Programacion_Avanzada/Assets/js/consultaPaciente.js
@@ -0,0 +1,32 @@
+﻿$(document).ready(function () {
+    $("#tablaAjustes").hide();
+});
+
+function MostrarAjustes(ConsecutivoSeleccionado) {
+
+    $.ajax({
+        type: 'GET',
+        url: '/Consulta/ConsultarAjustes',
+        data: { Consecutivo: ConsecutivoSeleccionado },
+        dataType: 'json',
+        success: function (data) {
+
+            var lista = "";
+            $.each(data, function (index, value) {
+                lista += '<tr><td>' + value.Ajuste + '</td><td>' + value.AjusteRealizado + '</td></tr>';
+            })
+
+            $("#tAjustes").html(lista);
+            $("#tablaAjustes").show();
+
+        },
+        error: function (data) {
+
+            $("#tAjustes").html('<tr><td colspan="2">Sin Registros</td></tr>');
+            $("#tablaAjustes").show();
+
+            //$("#tablaAjustes").hide();
+        }
+    });
+
+}
