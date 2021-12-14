313ed676cc137c0f220154f8142f50f4c1003c2d
k3vin1995 <57731251+k3vin1995@users.noreply.github.com>
2021-12-13 21:20:45 -0600

    agregando direccion etl

---
+++ Entidades.ETL/DireccionETL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades.ETL
+{
+    public class DireccionETL
+    {
+        public long CodigoDireccion { get; set; }
+        public string Provincia { get; set; }
+        public string Canton { get; set; }
+        public string Distrito { get; set; }
+        public string resena { get; set; }
+    }
+}
